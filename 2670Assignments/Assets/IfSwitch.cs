00246070c1a63a28da5a1ecf6b84a3439bc4ec79
Sarah Stewart <sstewartyankeefan@yahoo.com>
2016-09-16 20:42:31 -0600

    functions/ methods and if/switch

---
+++ 2670Assignments/Assets/IfSwitch.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfSwitch : MonoBehaviour
+{
+
+    public int food = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            satisfyHunger();
+        }
+    }
+
+    void satisfyHunger()
+    {
+        food = Random.Range(1, 4);
+
+        switch (food)
+        {
+
+            case 1:
+                Debug.Log("You found a banana");
+                break;
+            case 2:
+                Debug.Log("You found an apple");
+                break;
+            case 3:
+                Debug.Log("You found a donut");
+                break;
+            case 4:
+                Debug.Log("You found a pear");
+                break;
+
+
+        }
+    }
+    }
---
+++ 2670Assignments/Assets/Variablesfunctions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variablesfunctions : MonoBehaviour
+{
+
+    int myInt = 10;
+
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = number * 4;
+        return ret;
+    }
+}
