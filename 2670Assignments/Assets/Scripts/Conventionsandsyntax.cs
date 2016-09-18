4cd345e4120a24642e1ecb4093e541dc4e1d8f4f
Sarah Stewart <sstewartyankeefan@yahoo.com>
2016-09-17 21:13:25 -0600

    Conventions and Syntax

---
+++ 2670Assignments/Assets/Scripts/Conventionsandsyntax.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Conventionsandsyntax : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 10f)
+        {
+            Debug.Log("I seem to be falling forever...");
+        }
+    }
+}
