21360f90c8629610f1c12cb49ffd9545f2eca626
Sarah Stewart <sstewartyankeefan@yahoo.com>
2016-09-20 20:29:19 -0600

    loops

---
+++ 2670Assignments/Assets/Scripts/Loops.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour
+{
+    int numOfStarsInPouch = 5;
+
+    // Use this for initialization
+    void Start ()
+    {
+	    while(numOfStarsInPouch > 0)
+        {
+            Debug.Log("I caught a star!");
+            numOfStarsInPouch--;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
