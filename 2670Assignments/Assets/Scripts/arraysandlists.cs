0a19c74f669434a1ac84452134388aea4b814494
Sarah Stewart <sstewartyankeefan@yahoo.com>
2016-09-23 20:17:59 -0600

    arrays

---
+++ 2670Assignments/Assets/Scripts/arraysandlists.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arraysandlists : MonoBehaviour
+{
+    public GameObject[] weapons;
+
+	// Use this for initialization
+	void Start ()
+    {
+        weapons = GameObject.FindGameObjectsWithTag("Weapon");
+
+        for(int i =0; i <weapons.Length; i++)
+        {
+            Debug.Log("Weapon number " + i + " is named " +weapons[i].name);
+
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
